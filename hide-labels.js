9637e1168b754d75dcd69e7f3a21f53fd85ee6bb
Mike Solomon <msolomon@thrillist.com>
2017-02-02 14:54:11 -0500

    give the page a second to load the cards

--- hide-labels.js
+++ hide-labels.js
@@ -1,3 +1,5 @@
-$('.ghx-extra-field-content').filter(function() {
-    return $(this).text() === "None";
-}).css("display", "none");
+setTimeout(function() {
+  $('.ghx-extra-field-content').filter(function() {
+      return $(this).text() === "None";
+  }).css("display", "none");
+}, 1000) //give the page a second to load the cards
