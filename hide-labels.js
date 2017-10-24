088518d9f2894d211b06b21c300e52bda2656c1e
Mike Solomon <msolomon@thrillist.com>
2017-10-24 09:12:33 -0400

    hide empty labels in the backlog too

--- hide-labels.js
+++ hide-labels.js
@@ -1,4 +1,7 @@
 var style = document.createElement('style')
 style.type = 'text/css'
+// hide empty labels on board view
 style.innerHTML = '.ghx-issue .ghx-extra-fields .ghx-fa {display: none;}'
+// hide empty labels in backlog view
+style.innerHTML += 'span[data-tooltip="Labels: None"] {visibility: hidden}'
 document.getElementsByTagName('head')[0].appendChild(style)
