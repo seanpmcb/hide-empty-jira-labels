e98bf89bcbed4a4cb2162d98049f40dc4af16a2d
Mike Solomon <msolomon@thrillist.com>
2017-11-03 09:52:57 -0400

    style labels in backlog to be smaller

--- hide-labels.js
+++ hide-labels.js
@@ -5,3 +5,7 @@ style.innerHTML = '.ghx-issue .ghx-extra-fields .ghx-fa {display: none;}'
 // hide empty labels in backlog view
 style.innerHTML += 'span[data-tooltip="Labels: None"] {visibility: hidden}'
 document.getElementsByTagName('head')[0].appendChild(style)
+// style labels in backlog with smaller text
+$(document).ready(function() {
+  $('.ghx-extra-field-content').wrap("<small />");
+});
