34ece5db272090fb9bbb8926a98bce30ff0b430d
Sean P. McBride <seanpmcb@seans-laptop.lan>
2023-05-15 18:57:26 -0400

    fix: Get this working again

--- hide-labels.js
+++ hide-labels.js
@@ -1,11 +1,5 @@
 var style = document.createElement('style')
 style.type = 'text/css'
-// hide empty labels on board view
-style.innerHTML = '.ghx-issue .ghx-extra-fields .ghx-fa {display: none;}'
-// hide empty labels in backlog view
-style.innerHTML += 'span[data-tooltip="Labels: None"] {visibility: hidden}'
-document.getElementsByTagName('head')[0].appendChild(style)
-// style labels in backlog with smaller text
-$(document).ready(function() {
-  $('.ghx-extra-field-content').wrap("<small />");
-});
+// hide empty labels on board view and backlog view
+style.innerHTML = 'span[data-tooltip="Labels: None"] {visibility: hidden}'
+document.head.appendChild(style)
